d6eac2f17a70434066a53037cb0b376f151e8fa9
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-09-08 21:32:40 -0300

    Update ejercicio4.js

--- ejercicios-practicos/ejercicio4.js
+++ ejercicios-practicos/ejercicio4.js
@@ -2,11 +2,23 @@ let anio_nacimiento = prompt("Ingrese año de nacimiento");
 
 let anio_actual=new Date().getFullYear();
 
-if (anio_actual - anio_nacimiento < 18 ){
-    console.log("No puede ingresar al sitio")
+if (anio_nacimiento === ""){
+    console.log("Debe ingresar el año de nacimiento");
 }
-else {
-console.log("Es mayor de edad, puede ingresar al sitio")
+else if(anio_nacimiento <= 1900){
+    console.log("Año inválido")
 }
+else if(anio_nacimiento > anio_actual){
+    console.log("El año ingresado no es correcto")
+}
+else if(1900 < anio_nacimiento < anio_actual){
+    if (anio_actual - anio_nacimiento <18){
+        window.alert("No puede ingresar al sitio")
 
+    }
+    else{
+        window.alert("Es mayor de edad, puede ingresar al sitio")
+
+    }
+}
 
