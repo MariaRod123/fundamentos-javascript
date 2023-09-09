bb2b5b12ce4f8d7eba70dfbd15784ab6164f4d29
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-09-08 23:22:29 -0300

    Agregado ejercicio3

---
+++ ejercicios-practicos/ejercicio3.js
@@ -0,0 +1,25 @@
+
+let pasaporte_vigente= prompt ("¿Tiene pasaporte vigente?: 1-Si  2-No");
+
+let test_pcr_negativo=prompt ("¿tiene test pcr negativo?: 1-Si  2-No");
+
+let pasaje=prompt("¿tiene pasaje de la aerolinea?: 1-Si  2-No");
+
+if (pasaporte_vigente==="1" && test_pcr_negativo==="1" && pasaje==="1"){        
+    console.log("Puede viajar")
+}
+else if(pasaporte_vigente==="2" || test_pcr_negativo==="2" || pasaje==="2" ){
+    console.log("Lo siento no puede viajar, no cumple con todos los requisitos");
+
+}
+
+else if( pasaporte_vigente ==="" || test_pcr_negativo==="" || pasaje ===""){
+    console.error("Debe ingresar alguna opcion");
+}
+else if(pasaporte_vigente!= "1" && pasaporte_vigente!="2 "|| test_pcr_negativo!="1" && test_pcr_negativo!="2" || pasaje!="1" && pasaje!="2"){
+    console.error("La opcion ingresada es incorrecta");
+
+}
+
+
+
