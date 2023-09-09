1ebe2bea05364aa0eec63c91e16371c0b9da7760
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-09-09 14:32:31 -0300

    Agregado ejercicio 6

---
+++ ejercicios-practicos/ejercicio6.js
@@ -0,0 +1,17 @@
+
+const array1=[0, 1, 9, 20, -6, 3, 5.2];
+const array2=[7, -3, 6, 9, 12, 4, 0.5];
+
+function Sumatoria(array1, array2){
+    if (array1.length === array2.length){
+        const sumatoria=[];
+        for (let i = 0; i < array1.length; i++) {
+            sumatoria.push(array1[i] + array2[i]);
+        }
+        return sumatoria;
+    } else {
+        console.log("La longitud de los array no coincide");
+    }        
+}
+
+console.log(Sumatoria(array1, array2));
