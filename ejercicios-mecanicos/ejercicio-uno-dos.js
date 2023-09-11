df1e1903761735394ae52357a03fd13a0dbcdcb1
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-09-11 14:45:24 -0300

    Add files via upload

---
+++ ejercicios-mecanicos/ejercicio-uno-dos.js
@@ -0,0 +1,16 @@
+/* 1- Define 5 variables con nombres distintos usando let var y const*/ 
+let perro="Toby";
+var temperatura=16.9;
+const pi=3.14159;
+let dia=12;
+const elementos=["naranja", 20, null ]
+
+
+
+/*2- Define 5 variables con nombres distintos y tipos de datos diferentes utlizando let var y const */
+
+var nombre=undefined;
+let correo="correo@mail.com";
+const numero=6;
+let telefono=null;
+const variable=false;
